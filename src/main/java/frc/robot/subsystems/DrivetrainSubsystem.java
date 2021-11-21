4942d71d2f46b532de4b47027dc30755c64f376c
danielbrownmsm <56051347+danielbrownmsm@users.noreply.github.com>
2021-11-20 21:05:32 -0600

    made it actually work after some testing, removed the speed limiting

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -11,10 +11,10 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class DrivetrainSubsystem extends SubsystemBase {
     private SpeedControllerGroup leftSide;
     private WPI_TalonFX left1Motor;
-    private WPI_TalonFX left2Motor;
+    //private WPI_TalonFX left2Motor;
 
     private WPI_TalonFX right1Motor;
-    private WPI_TalonFX right2Motor;
+    //private WPI_TalonFX right2Motor;
     private SpeedControllerGroup rightSide;
 
     private DifferentialDrive drivetrain;
@@ -22,13 +22,13 @@ public class DrivetrainSubsystem extends SubsystemBase {
     //TODO add shuffleboard support
 
     public DrivetrainSubsystem() {
-        left1Motor = new WPI_TalonFX(2);
-        left2Motor = new WPI_TalonFX(3);
-        leftSide = new SpeedControllerGroup(left1Motor, left2Motor);
+        left1Motor = new WPI_TalonFX(1);
+        //left2Motor = new WPI_TalonFX(3);
+        leftSide = new SpeedControllerGroup(left1Motor);
 
-        right1Motor = new WPI_TalonFX(0);
-        right2Motor = new WPI_TalonFX(1);
-        rightSide = new SpeedControllerGroup(right1Motor, right2Motor);
+        right1Motor = new WPI_TalonFX(2);
+        //right2Motor = new WPI_TalonFX(1);
+        rightSide = new SpeedControllerGroup(right1Motor);
 
         drivetrain = new DifferentialDrive(leftSide, rightSide);
     }
@@ -39,7 +39,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
      * @param rightSpeed right speed
      */
     public void tankDrive(double leftSpeed, double rightSpeed) {
-        drivetrain.tankDrive(leftSpeed / 4, rightSpeed / 4, false);
+        drivetrain.tankDrive(leftSpeed / 1, rightSpeed / 1, false);
     }
 
     /**
