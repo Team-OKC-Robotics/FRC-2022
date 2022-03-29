df948c602f986341a29d58331f641063353c754c
danielbrownmsm <daniel.brown@msmrockets.org>
2022-03-29 18:34:52 -0500

    reinstated drive off line auto

---
+++ src/main/java/frc/robot/autos/DriveOffLineAuto.java
@@ -0,0 +1,16 @@
+package frc.robot.autos;
+
+import frc.robot.commands.drivetrain.DriveCommand;
+import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.util.Auto;
+
+public class DriveOffLineAuto extends Auto {
+    public DriveOffLineAuto(DrivetrainSubsystem drivetrain) {
+        super(
+            "Drive Off Line Auto",
+            "An auto that drives off the line forwards (away from alliance station), scoring points",
+            2,
+            new DriveCommand(drivetrain, 50)
+        );
+    }
+}
