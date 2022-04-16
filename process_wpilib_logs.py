62499ca73c18cee181c0256678e337d766211289
danielbrownmsm <daniel.brown@msmrockets.org>
2022-04-16 15:28:55 -0500

    logs

--- process_wpilib_logs.py
+++ process_wpilib_logs.py
@@ -24,22 +24,22 @@ entries = {
     '"/shooter/output"':outputLog,
     '"/shooter/setpoint"':setpointLog,
 
-    # '"/intake/setpoint"':intakeSetpointLog,
-    # '"/intake/output"':intakeOutputLog,
-    # '"/intake/pos"':intakePosLog,
+    '"/intake/setpoint"':intakeSetpointLog,
+    '"/intake/output"':intakeOutputLog,
+    '"/intake/pos"':intakePosLog,
 
-    # '"/climber/leftPos"':leftPosLog,
-    # '"/climber/leftOutput"':leftOutputLog,
-    # '"/climber/leftTiltPos"':leftTiltPosLog,
-    # '"/climber/leftTiltOutput"':leftTiltOutputLog,
-    # '"/climber/rightPos"':rightPosLog,
-    # '"/climber/rightOutput"':rightOutputLog,
-    # '"/climber/rightTiltPos"':rightTiltPosLog,
-    # '"/climber/rightTiltOutput"':rightTiltOutputLog,
+    '"/climber/leftPos"':leftPosLog,
+    '"/climber/leftOutput"':leftOutputLog,
+    '"/climber/leftTiltPos"':leftTiltPosLog,
+    '"/climber/leftTiltOutput"':leftTiltOutputLog,
+    '"/climber/rightPos"':rightPosLog,
+    '"/climber/rightOutput"':rightOutputLog,
+    '"/climber/rightTiltPos"':rightTiltPosLog,
+    '"/climber/rightTiltOutput"':rightTiltOutputLog,
 }
 
-log = r"C:\Users\teamo\Documents\GitHub\FRC-2022\logs\all\FRC_20220408_210622_OKTU_Q43.csv"
-# log = r"C:\Users\teamo\Documents\GitHub\FRC-2022\logs\all\FRC_20220402_215713.csv"
+# log = r"C:\Users\teamo\Documents\GitHub\FRC-2022\logs\all\FRC_20220408_210622_OKTU_Q43.csv"
+log = r"C:\Users\teamo\Documents\GitHub\FRC-2022\logs\all\FRC_20220416_201646.csv"
 with open(log) as f:
 # with open(r"C:\Users\teamo\Documents\GitHub\FRC-2022\logs\all\FRC_20220408_175227.csv") as f:
     log = f.read().split("\n")
@@ -71,25 +71,26 @@ plt.xlabel("Time (sec)")
 plt.ylabel("data")
 plt.legend(("pid output", "output", "rpm", "setpoint"))
 
-# plt.figure()
-# plt.plot(intakeSetpointLog[0], intakeSetpointLog[1])
-# plt.plot(intakeOutputLog[0], intakeOutputLog[1])
-# plt.plot(intakePosLog[0], intakePosLog[1])
-# plt.legend(("intake setpoint", "intake output", "intake pos"))
+plt.figure()
+plt.plot(intakeSetpointLog[0], intakeSetpointLog[1])
+plt.plot(intakeOutputLog[0], intakeOutputLog[1])
+plt.plot(intakePosLog[0], intakePosLog[1])
+plt.legend(("intake setpoint", "intake output", "intake pos"))
 
-# plt.figure()
-# plt.plot(leftOutputLog[0], leftOutputLog[1])
-# plt.plot(leftPosLog[0], leftPosLog[1])
-# plt.plot(leftTiltOutputLog[0], leftTiltOutputLog[1])
-# plt.plot(leftTiltPosLog[0], leftTiltPosLog[1])
+plt.figure()
+plt.plot(leftOutputLog[0], leftOutputLog[1])
+plt.plot(leftPosLog[0], leftPosLog[1])
+plt.plot(leftTiltOutputLog[0], leftTiltOutputLog[1])
+plt.plot(leftTiltPosLog[0], leftTiltPosLog[1])
 
-# plt.plot(rightOutputLog[0], rightOutputLog[1])
-# plt.plot(rightPosLog[0], rightPosLog[1])
-# plt.plot(rightTiltOutputLog[0], rightTiltOutputLog[1])
-# plt.plot(rightTiltPosLog[0], rightTiltPosLog[1])
-# plt.legend(("left output", "left pos", "left tilt output", "left tilt pos", "right output", "right pos", "right tilt output", "right tilt pos"))
+plt.plot(rightOutputLog[0], rightOutputLog[1])
+plt.plot(rightPosLog[0], rightPosLog[1])
+plt.plot(rightTiltOutputLog[0], rightTiltOutputLog[1])
+plt.plot(rightTiltPosLog[0], rightTiltPosLog[1])
+plt.legend(("left output", "left pos", "left tilt output", "left tilt pos", "right output", "right pos", "right tilt output", "right tilt pos"))
 
-# plt.show()
+plt.show()
+raise SystemExit
 
 def calculate(state, output, param):
     try:
